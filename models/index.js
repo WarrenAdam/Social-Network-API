d232e13beb935ab671f164796d0d704e211cfee6
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-09-19 00:01:46 -0400

    fixed model typo

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // Importing the User and Thought models from their respective files
-const User = require('./Users');
+const Users = require('./Users');
 const Thought = require('./Thought');
 // Exporting the User and Thought models as a single module for easy access in other parts of the application
 module.exports = {Thought, Users};
