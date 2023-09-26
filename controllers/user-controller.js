d8d4769ae5c3058d851dcc08d84ef10dc4ea9671
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-09-25 20:17:42 -0400

    added CRUDE operations for user

---
+++ controllers/user-controller.js
@@ -0,0 +1,90 @@
+const { User } = require('../models');
+
+const UserController = {
+  // Get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+
+  // Get one user by ID
+  getUserById(req, res) {
+    User.findById(req.params.userId)
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+  
+  // Create a user
+  createUser(req, res) {
+    User.create(req.body)
+      .then(userData => res.json(userData))
+      .catch(err => res.status(500).json(err));
+  },
+
+  // Update user by ID
+  updateUserById(req, res) {
+    User.findOneAndUpdate(req.params.id, req.body, { new: true })
+      .then(userData => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User does not exist' });
+        }
+        res.json(userData);
+      })
+      .catch(err => res.status(500).json(err));
+  },
+
+  // Delete user
+  deleteUserById(req, res) {
+    User.findOneAndDelete(req.params.id)
+      .then(userData => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User does not exist' });
+        }
+        res.json({ message: 'User deleted' });
+      })
+      .catch(err => res.status(500).json(err));
+  },
+
+  // Add friend to list
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body.friendId || req.params.friendId} },
+      { new: true }
+    )
+      .then(userData => {
+        if (!userData) {
+          return res.status(404).json({ message: 'User does not exist' });
+        }
+        res.json(userData);
+      })
+      .catch(err => res.status(500).json(err));
+  },
+
+
+  // Remove friend from list
+
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: "No user with this id!" });
+        }
+        const removed = !dbUserData.friends.includes(params.friendId);
+        if (removed) {
+          res.json({ message: "Friend removed!", dbUserData });
+        } else {
+          res.json(dbUserData);
+        }
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+
+module.exports = UserController;
